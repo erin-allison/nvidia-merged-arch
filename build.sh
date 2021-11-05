a22eb92f6f9654f5c8e0ed746c422333009d6c78
Erin Allison <erin@eallison.us>
2021-11-04 21:30:28 -0500

    Add `vgpu_unlock-rs`
    Signed-off-by: Erin Allison <erin@eallison.us>

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+
 dir=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
 
 PKGDEST="${dir}/out" makepkg -p PKGBUILD.nvidia-merged
+PKGDEST="${dir}/out" makepkg -p PKGBUILD.vgpu_unlock-rs
